26a676fd29187a7275d7f48dd71898ed6439b01b
DiaraMar <diarranice@gmail.com>
2022-07-12 15:04:00 +0200

    commit

--- src/outLine/Application.java
+++ src/outLine/Application.java
@@ -4,7 +4,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("test Docker");
 		for (int i =0; i<4; i++) {
 			System.out.println("Line  " + (i+1));
 			
