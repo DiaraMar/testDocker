b42923749c1fc4ddc4241a3a23cb378fda35aa70
DiaraMar <diarranice@gmail.com>
2022-07-12 14:52:11 +0200

    first commit

---
+++ src/outLine/Application.java
@@ -0,0 +1,15 @@
+package outLine;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i =0; i<4; i++) {
+			System.out.println("Line " + (i+1));
+			
+		}
+
+	}
+
+}
